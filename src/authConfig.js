08b47a0bbf65263febbc1f6dab739e5c2527dfcf
Gwynbleid85 <hegr.milos85@seznam.cz>
2024-06-15 10:55:39 +0200

    asdf

--- src/authConfig.js
+++ src/authConfig.js
@@ -1,13 +1,11 @@
-const authConfig = {
-    authority: 'https://some_text.zitadel.cloud/', //Replace with your issuer URL
-    client_id: 'ABC123@Project', //Replace with your client id
-    redirect_uri: 'http://localhost:3000/callback',
-    response_type: 'code',
-    scope: 'openid profile email',
-    post_logout_redirect_uri: 'http://localhost:3000/',
-    userinfo_endpoint: 'https://instance-some_text.zitadel.cloud/oidc/v1/userinfo', //Replace with your user-info endpoint
-    response_mode: 'query',
-    code_challenge_method: 'S256',
-  };
-
- export default authConfig;
+export const authConfig = {
+  authority: "https://lifeliqe-27azj8.zitadel.cloud", //Replace with your issuer URL
+  client_id: "271577186778059711@lifeliqe", //Replace with your client id
+  redirect_uri: "http://localhost:3000/callback",
+  response_type: "code",
+  scope: "openid profile email",
+  post_logout_redirect_uri: "http://localhost:3000",
+  userinfo_endpoint: "https://lifeliqe-27azj8.zitadel.cloud/oidc/v1/userinfo", //Replace with your user-info endpoint
+  response_mode: "query",
+  code_challenge_method: "S256",
+};
--- src/components/Callback.js
+++ src/components/Callback.js
@@ -1,53 +1,124 @@
-import React, { useEffect } from 'react';
-import authConfig from '../authConfig';
+import React, { useEffect, useState } from "react";
+import { authConfig } from "../authConfig";
 
-const Callback = ({ auth, setAuth, userManager, userInfo, setUserInfo, handleLogout }) => {
+const Callback = ({
+  auth,
+  setAuth,
+  userManager,
+  userInfo,
+  setUserInfo,
+  handleLogout,
+}) => {
+  const [accessToken, setAccessToken] = useState("");
 
   useEffect(() => {
     if (auth === null) {
-      userManager.signinRedirectCallback().then((user) => {
-        if (user) {
-          setAuth(true);
-          const access_token = user.access_token;
-          // Make a request to the user info endpoint using the access token
-          fetch(authConfig.userinfo_endpoint, {
-            headers: {
-              'Authorization': `Bearer ${access_token}`
-            }
-          })
-            .then(response => response.json())
-            .then(userInfo => {
-              setUserInfo(userInfo);
-            });
-        } else {
+      userManager
+        .signinRedirectCallback()
+        .then((user) => {
+          if (user) {
+            console.log(user);
+            setAccessToken(user.access_token);
+            setAuth(true);
+            const access_token = user.access_token;
+            // Make a request to the user info endpoint using the access token
+            fetch(authConfig.userinfo_endpoint, {
+              headers: {
+                Authorization: `Bearer ${access_token}`,
+              },
+            })
+              .then((response) => response.json())
+              .then((userInfo) => {
+                console.log(userInfo);
+                setUserInfo(userInfo);
+              });
+          } else {
+            setAuth(false);
+          }
+        })
+        .catch((error) => {
           setAuth(false);
-        }
-      }).catch((error) => {
-        setAuth(false);
-      });
+        });
     }
   }, [auth, userManager, setAuth]);
 
+  const [authorized, setAuthorized] = useState("");
+  const [authorizedJWT, setAuthorizedJWT] = useState("");
+
+  const testAuth = () => {
+    setAuthorized("Loading...");
+    fetch("http://localhost:5063/test/authorize", {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+      .then((response) => {
+        console.log(response);
+        if (response.status === 200) {
+          setAuthorized("YES");
+        } else {
+          setAuthorized("NO");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        setAuthorized("NO");
+      });
+  };
+  const testAuthJWT = () => {
+    setAuthorizedJWT("Loading...");
+    fetch("http://localhost:5063/test/authorize/jwt", {
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+    })
+      .then((response) => {
+        console.log(response);
+        if (response.status === 200) {
+          setAuthorizedJWT("YES");
+        } else {
+          setAuthorizedJWT("NO");
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+        setAuthorizedJWT("NO");
+      });
+  };
 
   if (auth === true && userInfo) {
     return (
       <div>
         <h1>Welcome, {userInfo.name}!</h1>
         <h2>Your ZITADEL Profile Information</h2>
-        <h3>Name:  {userInfo.name}</h3>
+        <h3>Name: {userInfo.name}</h3>
         <h3>Email: {userInfo.email}</h3>
-        <h3>Email Verified: {userInfo.email_verified? "Yes": "No"}</h3>
+        <h3>Email Verified: {userInfo.email_verified ? "Yes" : "No"}</h3>
         <h3>Locale: {userInfo.locale}</h3>
+        <h2>Authorized basic: {authorized}</h2>
+        <h2>Authorized JWT: {authorizedJWT}</h2>
 
         <button onClick={handleLogout}>Log out</button>
+        <button
+          onClick={() => {
+            testAuth();
+          }}
+        >
+          Test auth basic
+        </button>
+
+        <button
+          onClick={() => {
+            testAuthJWT();
+          }}
+        >
+          Test auth JWT
+        </button>
       </div>
     );
+  } else {
+    return <div>Loading... {auth}</div>;
   }
-  else {
-      return <div>Loading...</div>;
-    }
-
 };
 
 export default Callback;
-
